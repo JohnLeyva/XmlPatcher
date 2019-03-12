c381780ce78daddd4452a196d4fc9f0edf283118
John Leyva <John.Leyva@purplebricks.com>
2019-03-12 20:57:32 +0000

    init

---
+++ XmlPatcher/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace XmlPatcher
+{
+    public class Class1
+    {
+        int GetXmlNodeLevel(XmlNode xmlNode)
+        {
+            int currentLevel = 0;
+            while (xmlNode != null) {
+                xmlNode = xmlNode.ParentNode;
+                currentLevel++;
+            }
+            return currentLevel;
+        }
+
+        public XmlWhitespace CreateXmlWhitespace(XmlNode xmlNode)
+        {
+            var result = xmlNode.ChildNodes.OfType<XmlWhitespace>()
+                    .OrderBy( x => x.Length ).FirstOrDefault();
+            if (result != null)
+                return (XmlWhitespace)result.Clone();
+
+            int length = GetXmlNodeLevel(xmlNode) * 2;
+            return xmlNode.OwnerDocument.CreateWhitespace("".PadLeft(length));
+        }
+    }
+}
