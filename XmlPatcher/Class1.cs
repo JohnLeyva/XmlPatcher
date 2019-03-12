2624a05a53279ed1a964defbd5c65112efed7853
John Leyva <John.Leyva@purplebricks.com>
2019-03-12 23:13:42 +0000

    1.0.3

--- XmlPatcher/Class1.cs
+++ XmlPatcher/Class1.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 
 namespace XmlPatcher
 {
-    public class Class1
+    public static class Class1
     {
-        int GetXmlNodeLevel(XmlNode xmlNode)
+        static int GetXmlNodeLevel(this XmlNode xmlNode)
         {
             int currentLevel = 0;
             while (xmlNode != null) {
@@ -16,15 +17,50 @@ namespace XmlPatcher
             return currentLevel;
         }
 
-        public XmlWhitespace CreateXmlWhitespace(XmlNode xmlNode)
+        public static XmlWhitespace CreateXmlWhitespace(XmlNode xmlNode)
         {
             var result = xmlNode.ChildNodes.OfType<XmlWhitespace>()
                     .OrderBy( x => x.Length ).FirstOrDefault();
             if (result != null)
                 return (XmlWhitespace)result.Clone();
 
-            int length = GetXmlNodeLevel(xmlNode) * 2;
+            int length = xmlNode.GetXmlNodeLevel() * 2;
             return xmlNode.OwnerDocument.CreateWhitespace("".PadLeft(length));
+        }
+
+        public static IEnumerable<XmlNode> GetNodes(this XmlNode xmlNode, IEnumerable<string> parts)
+        {
+            var nodes = new XmlNode[] { xmlNode };
+            foreach (var part in parts)
+            {
+                nodes = nodes.SelectMany(x => x.ChildNodes.OfType<XmlElement>()
+                   .Where(i => i.LocalName == part)).OfType<XmlElement>().ToArray();
+            }
+            return nodes;
+        }
+
+        public static IEnumerable<XmlNode> GetNodes(this XmlNode xmlNode, params string[] parts)
+        {
+            return xmlNode.GetNodes(parts);
+        }
+
+        public static XmlElement EnsureXmlElement(this XmlElement xmlNode, string propName, string propValue)
+        {
+            var result = (XmlElement)xmlNode.GetNodes(propName).FirstOrDefault();
+            if (result == null) {
+                result = xmlNode.OwnerDocument.CreateElement(propName);
+                XmlWhitespace xmlSpace = CreateXmlWhitespace(xmlNode);
+                if (xmlNode.LastChild.NodeType == XmlNodeType.Whitespace) {
+                    xmlNode.InsertBefore(xmlSpace, xmlNode.LastChild);
+                    xmlNode.InsertBefore(result, xmlNode.LastChild);
+                }
+                else {
+                    xmlNode.AppendChild(xmlSpace);
+                    xmlNode.AppendChild(result);
+                }
+            }
+            result.InnerText = propValue;
+            return result;
         }
     }
 }
